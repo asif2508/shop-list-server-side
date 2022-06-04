46cc79f1e9709fe302c3d5489a46b7b7a585f858
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-05 04:17:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// using middletier
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://todo-user:lfWlplLC7qRiWKRb@cluster0.uucsy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
