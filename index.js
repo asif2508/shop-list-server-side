ff523cd2059ffafbe37f96c3e0222d009539469e
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-05 22:29:59 +0600

    delete api and post api added

--- index.js
+++ index.js
@@ -9,13 +9,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://todo-user:lfWlplLC7qRiWKRb@cluster0.uucsy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dbuser:rZ3AYgEioe0dEMKp@cluster0.xmh9o.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        
+        const shopCollection = client.db("shoplistdb").collection("shops");
+        app.get('/shops', async(req, res)=>{
+            const query = {};
+            const cursor = shopCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.post('/shops', async(req, res)=>{
+            const data = req.body;
+            const result = await shopCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.delete('/shops/:_id', async (req, res) => {
+            const id = req.params._id;
+            const query = { _id: ObjectId(id) }
+            const result = await shopCollection.deleteOne(query);
+            res.send(result);
+        })
     } finally {
         // await client.close();
     }
