aba21a6ca8aff963d967f10b78012180c05fdae0
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-06 03:25:14 +0600

    FINAL COMMIT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://dbuser:rZ3AYgEioe0dEMKp@cluster0.xmh9o.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmh9o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
