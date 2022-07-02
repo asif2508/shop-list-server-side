4fdeb971827b734d457675c00f508b503db0993d
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-03 02:51:28 +0600

    shows collection added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const shopCollection = client.db("shoplistdb").collection("shops");
+        const showsCollection = client.db("shoplistdb").collection("shows");
         app.get('/shops', async(req, res)=>{
             const query = {};
             const cursor = shopCollection.find(query);
@@ -32,6 +33,19 @@ async function run() {
             const id = req.params._id;
             const query = { _id: ObjectId(id) }
             const result = await shopCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.get('/shows', async(req, res)=>{
+            const query = {};
+            const cursor = showsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.delete('/shops/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { id: ObjectId(id) }
+            const result = await showsCollection.findOne(query);
             res.send(result);
         })
     } finally {
